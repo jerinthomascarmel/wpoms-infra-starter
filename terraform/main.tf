92428c56bd5c09c32d5efa0d64585ed2d5c91fa0
jerinthomascarmel <115214418+jerinthomascarmel@users.noreply.github.com>
2026-06-08 12:57:38 +0530

    Update ECR repository configuration with variables
    Refactor ECR repository configuration to use a variable for multiple repositories and add output for repository URLs.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,17 @@ terraform {
 provider "aws" {
 }
 
-resource "aws_ecr_repository" "wpoms-web" {
-  name                 = "wpoms-web"
+variable "ecr_repos" {
+  default = ["my-app-frontend", "my-app-backend"]
+}
+
+resource "aws_ecr_repository" "repos" {
+  for_each             = toset(var.ecr_repos)
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+}
+
+output "repo_urls" {
+  for_each = toset(var.ecr_repos)
+  value = aws_ecr_repository.repos[each.value].repository_url
 }
