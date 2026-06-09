9c7b5886a33a611ac09c991ec6f504550cad6eee
jerinthomascarmel <115214418+jerinthomascarmel@users.noreply.github.com>
2026-06-09 22:15:25 +0530

    Add new ECR repository resource 'jerin/new'
    Added a new ECR repository resource for 'jerin/new'.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,16 @@ variable "ecr_repos" {
   default = ["jerin/wpoms-web", "jerin/wpoms-admin"]
 }
 
+
 resource "aws_ecr_repository" "repos" {
   for_each             = toset(var.ecr_repos)
   name                 = each.value
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_repository" "new_repo" {
+  name = "jerin/new"
+} 
 
 output "repo_urls" {
   value = { for name, repo in aws_ecr_repository.repos : name => repo.repository_url }
