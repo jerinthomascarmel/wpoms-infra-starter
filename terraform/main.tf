9b05a002d2d0e8f2fc9126cf44a02e9e0854c183
jerinthomascarmel <115214418+jerinthomascarmel@users.noreply.github.com>
2026-06-09 23:05:21 +0530

    Add output for EC2 public IP in main.tf
    Added output for EC2 public IP address.

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,7 @@ resource "aws_instance" "terraform-jerin-ec2" {
 output "repo_urls" {
   value = { for name, repo in aws_ecr_repository.repos : name => repo.repository_url }
 }
+
+output "ec2-public-ip" {
+  value = aws_instance.terraform-jerin-ec2.public_ip
+}
