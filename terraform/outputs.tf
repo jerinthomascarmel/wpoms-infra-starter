32181805a8393e304e91c3363ecc7119e36d5134
jerinthomascarmel <115214418+jerinthomascarmel@users.noreply.github.com>
2026-06-11 08:21:11 +0530

    Add outputs for ECR repository URLs and EC2 public IP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,7 @@
+output "ecr_repo_urls" {
+  value = { for name, repo in aws_ecr_repository.repos : name => repo.repository_url }
+}
 
+output "ec2-public-ip" {
+  value = aws_instance.terraform-jerin-ec2.public_ip
+}
