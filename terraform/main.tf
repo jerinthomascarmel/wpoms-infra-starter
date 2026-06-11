1ed4f35c3de708f7f1edd76ddda13c7e30b64c63
jerinthomascarmel <115214418+jerinthomascarmel@users.noreply.github.com>
2026-06-11 08:36:33 +0530

    Modify security group and add ingress/egress rules
    Updated security group to allow HTTP, HTTPS, and SSH access. Added ingress and egress rules for TLS traffic.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,44 @@ resource "aws_ecr_repository" "repos" {
   image_tag_mutability = "MUTABLE"
 }
 
+resource "aws_security_group" "terraform_jt_sg" {
+  name        = "terraform jt security group"
+  description = "Allow HTTP , HTTPS , SSH Access and all outbout access"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform_jt_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 resource "aws_security_group" "terraform_jt_sg" {
   name        = "terraform-ec2-sg"
