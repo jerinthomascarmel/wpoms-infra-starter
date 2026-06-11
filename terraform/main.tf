3d8c7610ca4772e3ae0e927a74054c4a685c6b24
jerinthomascarmel <jerinthomascarmel@gmail.com>
2026-06-11 10:44:35 +0530

    added for each for inbound

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,33 +15,18 @@ resource "aws_security_group" "terraform_jt_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.terraform_jt_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+resource "aws_vpc_security_group_ingress_rule" "inbound" {
+  for_each = set(var.inbound_rules)
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.terraform_jt_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+  cidr_ipv4         = each.value.cidr_ipv4
+  from_port         = each.value.from_port
+  ip_protocol       = each.value.ip_protocol
+  to_port           = each.value.to_port
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.terraform_jt_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+resource "aws_vpc_security_group_egress_rule" "outbound" {
   security_group_id = aws_security_group.terraform_jt_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,26 @@
 variable "ecr_repos" {
   default = ["jerin/wpoms-web", "jerin/wpoms-admin"]
 }
+
+variable "inbound_rules" {
+  default = {
+    ssh = {
+      cidr_ipv4         = "0.0.0.0/0"
+      from_port         = 22
+      ip_protocol       = "tcp"
+      to_port           = 22
+    }
+    https = {
+      cidr_ipv4         = "0.0.0.0/0"
+      from_port         = 443
+      ip_protocol       = "tcp"
+      to_port           = 443
+    }
+    http = {
+      cidr_ipv4         = "0.0.0.0/0"
+      from_port         = 80
+      ip_protocol       = "tcp"
+      to_port           = 80
+    }
+  }
+}
